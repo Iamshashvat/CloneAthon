49e22107f28fcdebae5a29ce96da67e6ddb19884
Iamshashvat <sha.shashvat@gmail.com>
2020-09-24 23:15:39 +0530

    Airbnb inital commit

---
+++ static-airbnb-clone/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Home from "./components/pages/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import SearchPage from "./components/pages/SearchPage/SearchPage";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    // BEM
+    <div className='app'>
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path='/search'>
+            <SearchPage />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
