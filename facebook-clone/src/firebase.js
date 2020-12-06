d92fcae2ad9366d30f99d691e2f77c4a8527c7b1
shashvat Mohane <shashvat.mohane@gmail.com>
2020-12-06 17:26:24 +0530

    facebook clone added

---
+++ facebook-clone/src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSYoOFUHOiZ52FrQSd-1DguePeuV7RnYk",
+  authDomain: "clone-147b8.firebaseapp.com",
+  databaseURL: "https://clone-147b8.firebaseio.com",
+  projectId: "clone-147b8",
+  storageBucket: "clone-147b8.appspot.com",
+  messagingSenderId: "511454692366",
+  appId: "1:511454692366:web:d74751eccafc287e18a85f",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
+
